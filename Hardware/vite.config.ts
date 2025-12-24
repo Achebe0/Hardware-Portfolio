c1e5e9a619229b716922e06a38e2cf1e8b9d47c5
Achebe <achebeculpepper@gmail.com>
2025-12-24 00:48:42 -0500

    updated

--- Hardware/vite.config.ts
+++ Hardware/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/Hardware-Portfolio",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
